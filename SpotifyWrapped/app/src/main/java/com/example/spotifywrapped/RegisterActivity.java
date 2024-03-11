a8ecc1e4cdf8c60f10ad77dedf89286c928c7e8c
Rishab Paruchuri <rishparu@gmail.com>
2024-03-11 00:01:31 -0400

    finished user interface for register

---
+++ SpotifyWrapped/app/src/main/java/com/example/spotifywrapped/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.spotifywrapped;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+    private
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
