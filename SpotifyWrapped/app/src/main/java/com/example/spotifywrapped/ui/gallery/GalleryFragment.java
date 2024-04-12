e1ce2729e387f569a5f821b7fc9e341ba56f4055
prathampatel21 <prathampatel@MacBook-Air-66.local>
2024-04-12 00:40:27 -0400

    Redo

--- SpotifyWrapped/app/src/main/java/com/example/spotifywrapped/ui/gallery/GalleryFragment.java
+++ SpotifyWrapped/app/src/main/java/com/example/spotifywrapped/ui/gallery/GalleryFragment.java
@@ -1,16 +1,23 @@
 package com.example.spotifywrapped.ui.gallery;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.spotifywrapped.R;
 import com.example.spotifywrapped.databinding.FragmentGalleryBinding;
+import com.squareup.picasso.Picasso;
 
 public class GalleryFragment extends Fragment {
 
@@ -24,9 +31,38 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        String url = "https://www.billboard.com/wp-content/uploads/media/bruno-mars-thats-what-i-like-video-billboard-1548.jpg";
+        ImageView imageView = root.findViewById(R.id.imageView);
+        Picasso.get().load(url)
+                .placeholder(R.drawable.imgload)
+                .into(imageView);
+
+        MediaPlayer m = new MediaPlayer();
+
+        String songLink = "https://p.scdn.co/mp3-preview/93046e987d8c5bfdbeea2768ac1a8ecea17bd7e0?cid=1b5ff154cb554dbc9e189a18aff5d701";
+        startAudioStream(songLink, m);
+//        Log.d("userInfo", currentuser)
         final TextView textView = binding.textViewArtist1;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    public void startAudioStream(String url, MediaPlayer m) {
+        try {
+            Log.d("mylog", "Playing: " + url);
+            m.setAudioAttributes(
+                    new AudioAttributes
+                            .Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .build());
+            m.setDataSource(url);
+            m.prepare();
+            m.setVolume(1f, 1f);
+            m.setLooping(true);
+            m.start();
+        } catch (Exception e) {
+            Log.d("mylog", "Error playing in SoundHandler: " + e.toString());
+        }
     }
 
     @Override
